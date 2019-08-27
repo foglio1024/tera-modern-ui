05acfd8f38051b347f13e5a642eb9e2f06e95c78
Foglio1024 <foglio1024@gmail.com>
2019-08-27 14:58:04 +0200

    add log warning to inventory

--- Modern_InventoryWindow/index.js
+++ Modern_InventoryWindow/index.js
@@ -1,5 +1,9 @@
 module.exports = function InventoryWindow(m)
 {
-    if (m.majorPatchVersion >= 85) return;
+    if (m.majorPatchVersion >= 85)
+    {
+        m.warn('Mod disabled due to incompatibility with current patch. Wait for an update by the developer.');
+        return;
+    }
     m.installGPK("S1UI_InventoryWindow.gpk");
 }
