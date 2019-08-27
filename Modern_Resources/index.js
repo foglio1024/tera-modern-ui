1e972b6d86b45a1479c61670d322187fe1a2054c
Foglio1024 <foglio1024@gmail.com>
2019-08-27 15:20:57 +0200

    disable resources too for p85 and above

--- Modern_Resources/index.js
+++ Modern_Resources/index.js
@@ -1,4 +1,10 @@
 module.exports = function Resources(m) {
+    if (m.majorPatchVersion >= 85)
+    {
+        m.warn('Mod disabled due to incompatibility with current patch. Wait for an update by the developer.');
+        return;
+    }
+
     m.installGPK("S1UIRES_Skin.gpk");
     m.installGPK("S1UIRES_Component.gpk");
     m.installGPK("S1UIRES_Atlas.gpk");
