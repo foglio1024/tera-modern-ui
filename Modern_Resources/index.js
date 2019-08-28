a9493a2b38ee138072f620448a992cc9ee3030c5
Foglio1024 <foglio1024@gmail.com>
2019-08-28 14:56:18 +0200

    initial resources restyle

--- Modern_Resources/index.js
+++ Modern_Resources/index.js
@@ -1,11 +1,15 @@
-module.exports = function Resources(m) {
+module.exports = function Resources(m)
+{
     if (m.majorPatchVersion >= 85)
     {
-        m.warn('Mod disabled due to incompatibility with current patch. Wait for an update by the developer.');
-        return;
+        m.installGPK("85/S1UIRES_Skin.gpk");
+        m.installGPK("85/S1UIRES_Component.gpk");
+        m.installGPK("85/S1UIRES_Atlas.gpk");
     }
-
-    m.installGPK("S1UIRES_Skin.gpk");
-    m.installGPK("S1UIRES_Component.gpk");
-    m.installGPK("S1UIRES_Atlas.gpk");
+    else
+    {
+        m.installGPK("83/S1UIRES_Skin.gpk");
+        m.installGPK("83/S1UIRES_Component.gpk");
+        m.installGPK("83/S1UIRES_Atlas.gpk");
+    }
 }
