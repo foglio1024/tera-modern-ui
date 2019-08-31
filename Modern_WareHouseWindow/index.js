69a46ceb4802bb1b9d0b6469e68a68972b800b91
Foglio1024 <foglio1024@gmail.com>
2019-08-31 18:09:18 +0200

    keep p83 version for TW

--- Modern_WareHouseWindow/index.js
+++ Modern_WareHouseWindow/index.js
@@ -1,8 +1,11 @@
-module.exports = function WareHouseWindow(m) {
+module.exports = function WareHouseWindow(m)
+{
     if (m.majorPatchVersion >= 85)
     {
-        m.warn('Mod disabled due to incompatibility with current patch. Wait for an update by the developer.');
-        return;
+        m.installGPK("85/S1UI_WareHouseWindow.gpk");
     }
-    m.installGPK("S1UI_WareHouseWindow.gpk");
+    else
+    {
+        m.installGPK("83/S1UI_WareHouseWindow.gpk");
+    }
 }
