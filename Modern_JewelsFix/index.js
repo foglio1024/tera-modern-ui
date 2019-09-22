35a2bbaac48bafba162df95806e06cd7cd244ec5
Foglio1024 <foglio1024@gmail.com>
2019-09-22 16:49:13 +0200

    oops

--- Modern_JewelsFix/index.js
+++ Modern_JewelsFix/index.js
@@ -1,4 +1,4 @@
-module.exports = function PaperDoll(m) {
+module.exports = function JewelsFix(m) {
     m.installGPK("S1UI_PaperDoll.gpk");
     m.installGPK("S1UI_InventoryWindow.gpk");
     m.installGPK("Icon_Items.gpk");
