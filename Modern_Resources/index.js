9a8466838b3c44b25d9ba52fe4c669346c551d3a
Foglio1024 <foglio1024@gmail.com>
2019-10-15 15:10:30 +0200

    update modern-Resources

--- Modern_Resources/index.js
+++ Modern_Resources/index.js
@@ -2,8 +2,9 @@ module.exports = function Resources(m)
 {
     if (m.majorPatchVersion >= 86)
     {
-        m.warn('Mod disabled due to incompatibility with current patch. Wait for an update by the developer.');
-        return;
+        m.installGPK("85/S1UIRES_Skin.gpk");
+        m.installGPK("86/S1UIRES_Component.gpk");
+        m.installGPK("86/S1UIRES_Atlas.gpk");
     }
     if (m.majorPatchVersion >= 85)
     {
