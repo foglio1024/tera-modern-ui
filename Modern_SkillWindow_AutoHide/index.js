d59356b58805774fb0c9f866de3b64f590f9bdf7
Foglio1024 <foglio1024@gmail.com>
2020-01-28 14:34:17 +0100

    update SkillWindow

--- Modern_SkillWindow_AutoHide/index.js
+++ Modern_SkillWindow_AutoHide/index.js
@@ -1,10 +1,18 @@
 module.exports = function SkillWindow_AutoHide(m)
 {
-    if (m.majorPatchVersion > 86)
+    if (m.majorPatchVersion > 90)
     {
         m.warn('Mod disabled due to incompatibility with current patch. Wait for an update by the developer.');
         return;
     }
-    m.installGPK("S1UI_SkillWindow.gpk");
-    m.installGPK("S1UI_TestQuickSlot.gpk");
+    else if (m.majorPatchVersion >= 90) 
+    {
+        m.installGPK("90/S1UI_SkillWindow.gpk");
+        m.installGPK("90/S1UI_TestQuickSlot.gpk");
+    }
+    else
+    {
+        m.installGPK("86/S1UI_SkillWindow.gpk");
+        m.installGPK("86/S1UI_TestQuickSlot.gpk");
+    }
 }
