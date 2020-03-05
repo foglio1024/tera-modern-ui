7ddb590b0ee94f91c778e5a3cea1a92a0558e292
Foglio1024 <foglio1024@gmail.com>
2020-03-05 15:28:55 +0100

    update resources

--- Modern_Resources/index.js
+++ Modern_Resources/index.js
@@ -9,8 +9,7 @@ module.exports = function Resources(m)
     {
         m.installGPK("85/S1UIRES_Skin.gpk");
         m.installGPK("90/S1UIRES_Component.gpk");
-        m.warn('Mod partially disabled due to incompatibility with current patch. Wait for an update by the developer.');
-        // m.installGPK("86/S1UIRES_Atlas.gpk");
+        m.installGPK("91/S1UIRES_Atlas.gpk");
     }
     else if (m.majorPatchVersion >= 90)
     {
