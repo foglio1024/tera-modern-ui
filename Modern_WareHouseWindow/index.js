4aaeb2ac7ce03484fb3cad96238fa1795df310ca
Foglio1024 <foglio1024@gmail.com>
2020-05-11 17:05:18 +0200

    update bank

--- Modern_WareHouseWindow/index.js
+++ Modern_WareHouseWindow/index.js
@@ -1,11 +1,15 @@
 module.exports = function WareHouseWindow(m)
 {
-    if (m.majorPatchVersion >= 86)
+    if (m.majorPatchVersion > 93)
     {
         m.warn('Mod disabled due to incompatibility with current patch. Wait for an update by the developer.');
         return;
     }
-    if (m.majorPatchVersion >= 85)
+    if (m.majorPatchVersion >= 93)
+    {
+        m.installGPK("93/S1UI_WareHouseWindow.gpk");
+    }
+    else if (m.majorPatchVersion >= 85)
     {
         m.installGPK("85/S1UI_WareHouseWindow.gpk");
     }
