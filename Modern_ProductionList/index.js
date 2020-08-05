5195c18e5ab998706f2ccb6965ecd632774b2bc4
Foglio1024 <foglio1024@gmail.com>
2020-08-05 11:28:04 +0200

    change warning and automatically hide it on p97

--- Modern_ProductionList/index.js
+++ Modern_ProductionList/index.js
@@ -5,6 +5,8 @@ exports.ClientMod = class
         this.install = function(installer)
         {
             if(mod.clientInterface.arch == 'x64') return;
+            mod.warn("This mod will be disabled without showing any warning after 64-bit client patch hits. It will be automatically re-enabled when gpk mods will be supported again an update is released.");
+
             installer.gpk("S1UI_ProductionList.gpk");
         };
     }
