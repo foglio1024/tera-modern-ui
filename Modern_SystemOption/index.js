2f0afdcc24d55d53b30b4257cf93305169627077
Foglio1024 <foglio1024@gmail.com>
2020-08-05 11:31:47 +0200

    oopsie

--- Modern_SystemOption/index.js
+++ Modern_SystemOption/index.js
@@ -2,13 +2,16 @@ exports.ClientMod = class
 {
     constructor(mod)
     {
-        if (mod.clientInterface.arch == 'x64') return;
-        mod.warn("This mod will be disabled without showing any warning after 64-bit client patch hits. It will be automatically re-enabled when gpk mods will be supported again an update is released.");
-
-        if (mod.majorPatchVersion >= 85)
+        this.install = function (installer)
         {
-            return;
+            if (mod.clientInterface.arch == 'x64') return;
+            mod.warn("This mod will be disabled without showing any warning after 64-bit client patch hits. It will be automatically re-enabled when gpk mods will be supported again an update is released.");
+
+            if (mod.majorPatchVersion >= 85)
+            {
+                return;
+            }
+            installer.gpk("S1UI_SystemOption.gpk");
         }
-        installer.gpk("S1UI_SystemOption.gpk");
     }
 }
